8a02c87e8a64eeaeb8ba19dd6ced8c7754e02151
matthewleehess <matthewleehess@gmail.com>
2023-04-21 17:37:04 -0400

    update js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,7 @@
 
 //
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
+
 		});
 
 // wow
